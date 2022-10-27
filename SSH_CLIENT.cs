ceca126e085520f795280dd5f1b191aecdae2a53
Cytrix <code@cytrix.dev>
2022-10-27 18:37:37 -0400

    SSH CLIENT

--- SSH_CLIENT.cs
+++ SSH_CLIENT.cs
@@ -2,8 +2,14 @@ using Renci.SshNet;
 using Renci.SshNet.Common;
 using System.Text.RegularExpressions;
 
+// login with password
 SshClient Client = new SshClient("bandit.labs.overthewire.org", 2220, "bandit0", "bandit0");
 Client.Connect();
+
+// Login with Key
+PrivateKeyFile key = new PrivateKeyFile("file.key");
+SshClient Client = new SshClient("bandit.labs.overthewire.org", 2220, "bandit0", key);
+Client.Connect()
 
 SshCommand cmd = Client.CreateCommand("cat readme");
 cmd.Execute();
