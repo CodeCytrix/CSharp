86380829ff0547da280f52724820fdb98bb2752b
Cytrix <code@cytrix.dev>
2022-10-28 09:49:42 -0400

    Update HTTP_CLIENT.cs

--- HTTP/HTTP_CLIENT.cs
+++ HTTP/HTTP_CLIENT.cs
@@ -1,10 +1,36 @@
 using System.Net.Http;
+using System.Text;
+
+
+// Reqest GET Method
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri("http://some.server.com");
+HttpRequestMessage msg = new HttpRequestMessage(HttpMethod.Get, "/index.html");
+
+HttpResponseMessage httpResponse = await client.SendAsync(msg);
+httpResponse.EnsureSuccessStatusCode();
+String body = await httpResponse.Content.ReadAsStringAsync();
+Console.WriteLine(body);
+
+
+
+// Request with UserAgent
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri("http://some.server.com");
+HttpRequestMessage msg = new HttpRequestMessage(HttpMethod.Get, "/index.html");
+msg.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.24");
+
+HttpResponseMessage httpResponse = await client.SendAsync(msg);
+httpResponse.EnsureSuccessStatusCode();
+String body = await httpResponse.Content.ReadAsStringAsync();
+Console.WriteLine(body);
+
+
 
 // Request Head Method
 HttpClient client = new HttpClient();
-client.BaseAddress = new Uri("https://overthewire.org");
-HttpRequestMessage msg = new HttpRequestMessage(HttpMethod.Head, "/wargames/index.html");
-msg.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.24");
+client.BaseAddress = new Uri("http://some.server.com");
+HttpRequestMessage msg = new HttpRequestMessage(HttpMethod.Head, "/index.html");
 
 HttpResponseMessage httpResponse = await client.SendAsync(msg);
 httpResponse.EnsureSuccessStatusCode();
@@ -12,13 +38,15 @@ Console.WriteLine(httpResponse);
 
 
 
-// Reqest GET Method
+// Request with Basic Credential
 HttpClient client = new HttpClient();
-client.BaseAddress = new Uri("https://overthewire.org");
-HttpRequestMessage msg = new HttpRequestMessage(HttpMethod.Get, "/wargames/index.html");
-msg.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.24");
+client.BaseAddress = new Uri("http://some.server.com");
 
-HttpResponseMessage httpResponse = await client.SendAsync(msg);
+string credential = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes("user:password"));
+HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "/index.html");
+request.Headers.Add("Authorization", "Basic " + credential);
+
+HttpResponseMessage httpResponse = await client.SendAsync(request);
 httpResponse.EnsureSuccessStatusCode();
 String body = await httpResponse.Content.ReadAsStringAsync();
-Console.WriteLine(body);
+Console.WriteLine(body)
